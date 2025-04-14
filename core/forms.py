9e5c7e35caa8fd08c2e7cce500e6cbed0c19808a
mmd <8harifi@gmail.com>
2025-04-14 10:11:59 +0330

    fix: fix login and signup for custom user model with proper form handling

--- core/forms.py
+++ core/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class CustomSignupForm(UserCreationForm):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
+    full_name = forms.CharField()
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        fields = ['full_name', 'email', 'password1', 'password2']
 
