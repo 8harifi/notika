5827c14b9a5070033f50d86497c1e3c62373624a
mmd <8harifi@gmail.com>
2025-04-14 11:03:09 +0330

    feat: made the search bar fully functional

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,8 @@ from core.views import (
     favorites_view,
     upload_view,
     SignupView,
+    edit_profile_view,
+    download_document,
 )
 from django.contrib.auth.views import LoginView
 
@@ -14,10 +16,12 @@ app_name = 'core'
 
 urlpatterns = [
     # login required
-    path('search_results', view=search_results_view, name="search_results"),
+    path('search-results', view=search_results_view, name="search_results"),
     path('search', view=search_view, name="search"),
     path('favorites', view=favorites_view, name="favorites"),
     path('upload', view=upload_view, name="upload"),
+    path('edit-profile', view=edit_profile_view, name="edit_profile"),
+    path('download/<int:doc_id>/', view=download_document, name='download'),
 
     # login not required
     path('', view=landin_view, name="landing"),
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, FileResponse, Http404
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
@@ -7,6 +7,8 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from core.forms import CustomSignupForm
+from core.models import Favorite, Document
+from django.db.models import Q
 
 def landin_view(request):
     return render(request, "landing.html")
@@ -14,7 +16,14 @@ def landin_view(request):
 
 @login_required
 def search_results_view(request):
-    return render(request, "search_results.html")
+    query = request.GET.get('q', '')
+    results = Document.objects.filter(
+        Q(title__icontains=query) |
+        Q(description__icontains=query) |
+        Q(course__name__icontains=query)
+    )
+    return render(request, 'search_results.html', {'results': results, 'query': query})
+    # return render(request, "search_results.html")
 
 
 @login_required
@@ -24,12 +33,19 @@ def search_view(request):
 
 @login_required
 def favorites_view(request):
-    return render(request, "favorites.html")
+    user_id = request.user.id
+    user_fav_items = [x.document for x in Favorite.objects.filter(user_id=user_id)]
+    return render(request, "favorites.html", {'favorites': user_fav_items})
 
 
 @login_required
 def upload_view(request):
     return render(request, "upload.html")
+
+
+@login_required
+def edit_profile_view(request):
+    return render(request, "profile.html")
 
 
 class SignupView(CreateView):
@@ -41,4 +57,12 @@ class SignupView(CreateView):
         response = super().form_valid(form)
         login(self.request, self.object)
         return response
+
+@login_required
+def download_document(request, doc_id):
+    try:
+        doc = Document.objects.get(id=doc_id)
+        return FileResponse(doc.file.open(), as_attachment=True, filename=doc.file.name)
+    except Document.DoesNotExist:
+        raise Http404("Document not found.")
 
